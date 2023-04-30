e099d8fffda5b4b4b1eb85fff812c9d3a914bf4f
shixinzia <111138018+shixinzia@users.noreply.github.com>
2023-04-30 11:36:50 +0800

    Update Networks.java

--- src/main/java/io/github/sefiraat/networks/Networks.java
+++ src/main/java/io/github/sefiraat/networks/Networks.java
@@ -50,10 +50,10 @@ public class Networks extends JavaPlugin implements SlimefunAddon {
             return;
         }
 
-        getLogger().info("########################################");
-        getLogger().info("            Networks - 网络              ");
-        getLogger().info("       作者: Sefiraat 汉化: ybw0014      ");
-        getLogger().info("########################################");
+        getLogger().info("#############################################");
+        getLogger().info("               Networks - 网络               ");
+        getLogger().info(" 作者: Sefiraat 汉化: ybw0014 改版: shixinzia ");
+        getLogger().info("#############################################");
 
         saveDefaultConfig();
         tryUpdate();
