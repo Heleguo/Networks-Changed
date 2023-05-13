d94c0889ed768bb1cf6b9ac1d7e3617064dc65d2
shixinzia <111138018+shixinzia@users.noreply.github.com>
2023-05-13 21:59:06 +0800

    Update ListenerManager.java

--- src/main/java/io/github/sefiraat/networks/managers/ListenerManager.java
+++ src/main/java/io/github/sefiraat/networks/managers/ListenerManager.java
@@ -2,12 +2,14 @@ package io.github.sefiraat.networks.managers;
 
 import io.github.sefiraat.networks.Networks;
 import io.github.sefiraat.networks.listeners.ExplosiveToolListener;
+import io.github.sefiraat.networks.listeners.Shixinzia;
 import org.bukkit.event.Listener;
 
 public class ListenerManager {
 
     public ListenerManager() {
         addListener(new ExplosiveToolListener());
+        addListener(new ShiXinzia());
     }
 
     private void addListener(Listener listener) {
